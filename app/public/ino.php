0711df648581ecfae64a754525ee760bec9991c0
toyrik <toyrik@gmail.com>
2021-03-25 17:01:33 +0300

    Добавляем базу данных

--- app/public/ino.php
+++ app/public/ino.php
@@ -1,2 +1,15 @@
 <?php
+
+$server = 'mysql';
+$user = 'user';
+$password = 'password';
+$dbname = 'symfony';
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$dbname", $user, $password);
+    echo 'Подключение к базе успешно!';
+} catch (PDOExeption $e) {
+    echo "Error!!:" . $e->getMessage() . "<br>";
+}
+
 phpinfo();
+
